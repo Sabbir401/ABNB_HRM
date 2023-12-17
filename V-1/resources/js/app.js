621f6b29c28b818ffb85dd8311c10ab11180dcdc
Sabbir <ahmedsabbir401@gmail.com>
2023-12-17 14:19:37 +0600

    Install laravel and configure vue

---
+++ V-1/resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import app from './layout/app.vue'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+createApp(app)
+.mount('#app')
---
+++ V-1/resources/js/layout/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button
+      class="btn btn-primary"
+      data-bs-target="#collapseTarget"
+      data-bs-toggle="collapse"
+    >
+      Bootstrap collapse
+    </button>
+    <div class="collapse py-2" id="collapseTarget">
+      This is the toggle-able content!
+    </div>
+  </div>
+</template>
