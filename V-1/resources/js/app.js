5e325cfc7c594f2bc2c21b93d509e603a1f1bb54
Sabbir <ahmedsabbir401@gmail.com>
2023-12-18 12:45:35 +0600

    router

--- V-1/resources/js/app.js
+++ V-1/resources/js/app.js
@@ -1,10 +1,11 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
-import app from './layout/app.vue'
+
+import app from './layouts/app.vue'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import router from './router.js';
 
-createApp(app)
-.mount('#app')
+createApp(app).use(router).mount('#app')
--- V-1/resources/js/router.js
+++ V-1/resources/js/router.js
@@ -1,1 +1,24 @@
 import { createWebHistory, createRouter } from "vue-router";
+
+import home from './pages/home.vue';
+import about from './pages/about.vue';
+
+const routes = [
+    {
+        path : '/',
+        name: 'home',
+        component: home
+    },
+    {
+        path : '/about',
+        name: 'about',
+        component: about
+    },
+]
+
+const router = createRouter({
+    history : createWebHistory(),
+    routes
+});
+
+export default router;
